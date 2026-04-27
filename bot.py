30d1f73e01542103c07fd78da5b75aa436fc7592
matanovadilnura7-beep <matanovadilnura7@gmail.com>
2026-04-27 10:30:55 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,23 +2,27 @@ import telebot
 import requests
 
 TOKEN = "8717105037:AAGHh9ucJCqTAAS4uKuzJOFKDzYQ0_UPBbY"
-API_KEY = "AIzaSyBNQ4Ns9Z1ClMvKVVtgvscJbf7NiYXGtn0"
+API_KEY = "AIzaSyArww8FWEtiXfCEQwd1eyd3Z4D5L_0sVz0"
 
 bot = telebot.TeleBot(TOKEN)
 
 def ask_gemini(text):
-    url = f"https://generativelanguage.googleapis.com/v1beta/models/gemini-pro:generateContent?key={API_KEY}"
+    url = f"https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key={API_KEY}"
     
     data = {
         "contents": [{"parts": [{"text": text}]}]
     }
     
     response = requests.post(url, json=data)
-    return response.json()["candidates"][0]["content"]["parts"][0]["text"]
+    
+    try:
+        return response.json()["candidates"][0]["content"]["parts"][0]["text"]
+    except:
+        return "Xatolik yuz berdi (API javob bermadi)"
 
 @bot.message_handler(func=lambda message: True)
 def reply(message):
     javob = ask_gemini(message.text)
     bot.reply_to(message, javob)
 
-bot.polling()
+bot.polling(none_stop=True)
